020ae7cd07cf46548bef38246a81814ad9552e2d
Jacksoncraft859 <pollardjackson@outlook.com>
2023-11-15 07:37:40 -0500

    added other shit that i forgot to upload

---
+++ static/osana/osana.config.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";self.__osana$config={bare:`https://andromeda-osana-bare.vercel.app/`,prefix:"/~osana/",codec:self.__osana$bundle.codecs.none,files:{config:"/osana.config.js",client:"/osana.client.js",bundle:"/osana.bundle.js",worker:"/osana.worker.js"},blacklist:[]}})();
+//# sourceMappingURL=osana.config.js.map
